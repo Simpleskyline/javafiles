fa29d1d8c4795dff9d3295cf7247c8d756eb4cd3
Simpleskyline <telvintran@gmail.com>
2025-06-28 12:39:14 +0300

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,16 @@
-public class Arrays {
+/*public class Arrays {
     public static void main(String[] args) {
         // Declare and initialize an array of integers
         String[] cars = {"BMW", "Audi", "Mercedes", "Tesla", "Toyota"};
         System.out.println(cars[2]); // Accessing the third element (index 2)        }
+    }
+}
+*/
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[][] mynum = {{1, 2, 3, 4, 5},{6,7,8,9,10}};
+        // Accessing elements in a 2D array
+        System.out.println(mynum[1][3]); // Output: 9
     }
 }
