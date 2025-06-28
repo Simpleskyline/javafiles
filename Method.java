fd2bc04b4ef77f9975932b20a74dbf452e557e98
Simpleskyline <telvintran@gmail.com>
2025-06-28 12:59:45 +0300

    adds on

--- Method.java
+++ Method.java
@@ -1,5 +1,15 @@
-publi class Method{
-    public static public static void main(String[] args) {
-        
+// File: Method.java
+public class Method{
+     public static void main(String[] args) {
+        happyBirthday();
+         happyBirthday();
+         happyBirthday();
     }
+//a block of reusable code that is executed when called ()
+static void happyBirthday() {
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday dear friend!");
+        System.out.println("Happy Birthday to you!");
+    }   
 }
