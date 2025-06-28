5e9673068b7635f9c351fbc22a4640cd37f2ca73
Simpleskyline <telvintran@gmail.com>
2025-06-28 13:01:55 +0300

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method{
+     public static void main(String[] args) {
+        happyBirthday();
+         happyBirthday();
+         happyBirthday();
+    }
+//a block of reusable code that is executed when called ()
+static void happyBirthday() {
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday dear friend!");
+        System.out.println("Happy Birthday to you!");
+    }   
+}
