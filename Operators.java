b3664c9de1e40ea93dd866e3a478b3c66a723ab0
Simpleskyline <telvintran@gmail.com>
2025-06-29 18:43:14 +0300

    changes

---
+++ Operators.java
@@ -0,0 +1,6 @@
+public class Operators {
+public static void main(String args[]){
+ //!(logical not) It negates the result of the evaluation
+ System.out.println(!(3<10)); // True but note that (3>7) returns false
+}
+}
