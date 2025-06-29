16078a15de413852027d38e335568639cc24d16b
Simpleskyline <telvintran@gmail.com>
2025-06-29 17:52:13 +0300

    adds on

---
+++ Increment.java
@@ -0,0 +1,7 @@
+public class Increment {
+    public static void main(String []args){
+        int i=5;
+        System.out.println(i++); // Post-increment: prints 5, then increments i to 6
+        System.out.println(++i); // Pre-increment: increments i to 7, then prints 7
+    }
+}
