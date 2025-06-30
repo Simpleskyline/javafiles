2f27c995d86e5a2c5b413944bae7341f1bb55be2
Simpleskyline <telvintran@gmail.com>
2025-06-30 13:25:01 +0300

    adds on

---
+++ Printf.java
@@ -0,0 +1,6 @@
+public class Prinf{
+    public static void main(String[] args) {
+     //printf is a method used to format and print text to the console
+     System.out.printf("Your age is %d" ,25);
+    }
+}
