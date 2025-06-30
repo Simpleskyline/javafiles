1e6ad3f39744923cce042b55aba3bb9261d8dfbd
Simpleskyline <telvintran@gmail.com>
2025-06-30 15:29:43 +0300

    change

--- Arrays.java
+++ Arrays.java
@@ -1,14 +1,9 @@
-public class Arrays {
+public class Arrays{
     public static void main(String[] args) {
-        int[][][] cube = {
-            {
-                {1, 2}, {3, 4}
-            },
-            {
-                {5, 6}, {7, 8}
-            }
-        };
-
-        System.out.println("Access element [1][0][1]: " + cube[1][0][1]); // Output: 6
+        int [][] num = {{1, 2, 3},{4, 5, 6}};
+    
+    System.out.println(num[1][2]);
     }
-}
+    }
+    // This will print the value at the second row and third column of the 2D array
+    // which is 6.
