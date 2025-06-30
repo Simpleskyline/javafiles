12a74b401bf1e2593643eec4221a8fa142e4da97
Simpleskyline <telvintran@gmail.com>
2025-06-30 14:51:47 +0300

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,14 +1,31 @@
-public class Arrays {
-    public static void main(String[] args) {
-        int[][][] cube = {
-            {
-                {1, 2}, {3, 4}
-            },
-            {
-                {5, 6}, {7, 8}
-            }
-        };
+class Car {
+    String make;
+    String model;
 
-        System.out.println("Access element [1][0][1]: " + cube[1][0][1]); // Output: 6
+    Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    void display() {
+        System.out.println("Make: " + make + ", Model: " + model);
+    }
+}
+
+public class CarGrid {
+    public static void main(String[] args) {
+        Car[][] garage = new Car[2][2];
+
+        garage[0][0] = new Car("Toyota", "Corolla");
+        garage[0][1] = new Car("Honda", "Civic");
+        garage[1][0] = new Car("Ford", "Mustang");
+        garage[1][1] = new Car("Chevrolet", "Camaro");
+
+        System.out.println("Garage layout:");
+        for (int i = 0; i < garage.length; i++) {
+            for (int j = 0; j < garage[i].length; j++) {
+                garage[i][j].display();
+            }
+        }
     }
 }
