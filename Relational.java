3ed8f2fe2797d8de2ee51ec2aa2f4393ab43bbff
Simpleskyline <telvintran@gmail.com>
2025-07-02 19:19:19 +0300

    adds

---
+++ Relational.java
@@ -0,0 +1,12 @@
+public class Relational{
+    public static void main(String[] args){
+        int age = 25;
+        
+        //logical and operator
+        System.out.println(age >= 18 && age <= 40);
+
+        //logical or operator 
+        boolean isStudent = true;
+        boolean isLibraryMember = false;
+    }
+}
