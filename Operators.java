2078d864bc894abb211abf759bd1370530d2f058
Simpleskyline <telvintran@gmail.com>
2025-07-02 18:55:09 +0300

    .

--- Operators.java
+++ Operators.java
@@ -1,6 +1,9 @@
 public class Operators {
 public static void main(String args[]){
- //!(logical not) It negates the result of the evaluation
- System.out.println(!(3<10)); // True but note that (3>7) returns false
+int number1 = 100; // First number
+int number2 = 20; // Second number
+
+// Arithmetic Operators
+System.out.println(number1 / number2); // Division
 }
 }
