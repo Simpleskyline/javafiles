5634efe02850793dbfbd4c6596dc5b306f0c0c47
Simpleskyline <telvintran@gmail.com>
2025-07-03 09:47:00 +0300

    .

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+    public static void main(String[] args) {
+        char vowels[] = new char[5];
+        vowels[0] = 'a';
+        vowels[1] = 'e';
+        vowels[2] = 'i';
+        System.out.println(vowels[1]);
+    }
+    }
