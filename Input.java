2e0729bf8ccc7a5728d4d0f3c7b3207a57fbdb53
Simpleskyline <telvintran@gmail.com>
2025-07-03 09:31:14 +0300

    .

--- Input.java
+++ Input.java
@@ -3,10 +3,10 @@ public class Input{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("What is your name?");
+        System.out.print("What is your name?");
         String name = scanner.nextLine();
 
-       System.out.println(name);
+       System.out.println("Your name is " +name);
 
        scanner.close();
     }
