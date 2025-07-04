5f3d7367cef453ab78a42cd29a95aea23984467c
Simpleskyline <telvintran@gmail.com>
2025-07-04 13:22:49 +0300

    .

--- Array.java
+++ Array.java
@@ -1,4 +1,4 @@
-public class Array{
+/*public class Array{
     public static void main(String[] args) {
         char vowels[] = new char[5];
         vowels[0] = 'a';
@@ -6,4 +6,15 @@ public class Array{
         vowels[2] = 'i';
         System.out.println(vowels[1]);
     }
+    }*/
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+        System.out.println("First number: " + numbers[0]);
     }
+}
