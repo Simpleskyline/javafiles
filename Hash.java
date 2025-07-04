949717d860cf9e7c2f2c5bddce96a8b70ca770e3
Simpleskyline <telvintran@gmail.com>
2025-07-04 12:46:50 +0300

    .

--- Hash.java
+++ Hash.java
@@ -5,13 +5,19 @@ import java.util.HashMap;
 
 public class Hash {
     public static void main(String[] args) {
-        HashMap<String, Double> map = new HashMap<String, Double>();
+        HashMap<String, Double> map = new HashMap<>();
 
 
         map.put("apple", 0.50);
         map.put("banana", 0.30);
         map.put("orange", 0.60);
+        map.put("grape", 0.80);
 
-        System.out.println(map); 
+        //map.remove("apple"); // Remove the key "apple"
+
+        
+        //System.out.println(map.get("apple")); // Retrieve the value associated with the key "apple" 
+
+        System.out.println(map.containsKey("banana")); // Check if the key "banana" exists in the map
     }
 }
