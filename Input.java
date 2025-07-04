d288dd4351af3da4fc24a6bb92f8a31aab5ced34
Simpleskyline <telvintran@gmail.com>
2025-07-04 13:37:33 +0300

    .

--- Input.java
+++ Input.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class Input{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -8,4 +8,19 @@ public class Input{
        
        scanner.close();
     }
+}*/
+
+import java.util.Scanner;
+
+public class Input{
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+
+    // Prompt the user for their name
+    System.out.print("What is your name? ");
+    String name = scanner.nextLine();
+
+    // Print a greeting message
+    System.out.println("Hello, " + name + "!");
+}
 }
