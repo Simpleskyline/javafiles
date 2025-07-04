beb20af7e4319985b1c8935d5fcc03380c55fdd0
Simpleskyline <telvintran@gmail.com>
2025-07-04 13:30:01 +0300

    .

--- Mathe.java
+++ Mathe.java
@@ -1,6 +1,6 @@
-public class Math {
+public class Mathe {
     public static void main(String []args){
-       int result = Math.Round(1.1F);
+       int result = Math.round(1.1F);
          System.out.println("Rounded value: " + result);
     }
 }
