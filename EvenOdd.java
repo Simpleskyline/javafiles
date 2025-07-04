02e8dfe41fd08d453bb0eb31d8d6c87f19f3cb15
Simpleskyline <telvintran@gmail.com>
2025-07-04 11:49:34 +0300

    .

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
-
+// This program checks if a number is even or odd
 public class EvenOdd {
     public static void main(String[] args) {
+
+        // Create a Scanner object to read input from the user
         Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter a number
         System.out.print("Enter a number: ");
         int num = input.nextInt();
 
