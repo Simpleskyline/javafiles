c2f42de1ac941d108edb7da39a7ef0ae3293b802
Simpleskyline <telvintran@gmail.com>
2025-07-04 12:26:48 +0300

    .

--- Hash.java
+++ Hash.java
@@ -1,2 +1,10 @@
 //Hashmap = a data structure that stores key value pairs 
 //It does not  maintain the order of elements but is memory efficient
+
+import java.util.HashMap;
+
+public class Hash{
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap();
+    }
+}
