d7c0a3529c5a2a5f37d31847b541b9bb4714e796
Simpleskyline <telvintran@gmail.com>
2025-07-04 11:29:29 +0300

    .

--- Oop.java
+++ Oop.java
@@ -1,6 +1,10 @@
+import java.time.LocalDate;
+
 public class Oop{
+    public String name;
+    public LocalDate birthDay;
     public static void main(String[] args) {
         
-        
+
     }
 }
