666d85ccb81002b314a8f61704870156125e59e6
Simpleskyline <telvintran@gmail.com>
2025-07-04 11:45:03 +0300

    .

--- PrimeCheck.java
+++ PrimeCheck.java
@@ -1,8 +1,10 @@
+//declares a public class named PrimeCheck
 public class PrimeCheck {
     public static void main(String[] args) {
         int num = 29;
         boolean isPrime = true;
 
+        //Any number less than or equal to 1 is not a prime number.
         if (num <= 1) isPrime = false;
 
         for (int i = 2; i <= Math.sqrt(num); i++) {
